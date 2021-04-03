8c022fd081a17ef9375e47a0c695fe790b8ef27c
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-04-03 10:17:04 +0700

    add unnecessary 'h'

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,3 +1,5 @@
+import { h } from 'https://deno.land/x/sift@0.1.7/mod.ts'
+
 const Layout = ({ children }) => {
   return (
     <html lang="en">
--- components/SuperCenter.jsx
+++ components/SuperCenter.jsx
@@ -1,3 +1,5 @@
+import { h } from 'https://deno.land/x/sift@0.1.7/mod.ts'
+
 const SuperCenter = ({ children }) => {
   return <div className="flex justify-center items-center top-0 bottom-0 mx-auto">{children}</div>
 }
